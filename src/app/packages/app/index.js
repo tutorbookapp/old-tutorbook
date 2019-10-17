eefa0e9444e46d043a016c4521f6caf9c53884a9
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-10-17 10:20:21 -0700

    fix(firebase): manually initialize app

--- src/app/packages/app/index.js
+++ src/app/packages/app/index.js
@@ -66,6 +66,15 @@ const Data = require('data');
 class Tutorbook {
 
     constructor() {
+        firebase.initializeApp({
+            "projectId": "tutorbook-779d8",
+            "databaseURL": "https://tutorbook-779d8.firebaseio.com",
+            "storageBucket": "tutorbook-779d8.appspot.com",
+            "locationId": "us-central",
+            "apiKey": "AIzaSyCNaEj1Mbi-79cGA0vW48iqZtrbtU-NTh4",
+            "authDomain": "tutorbook-779d8.firebaseapp.com",
+            "messagingSenderId": "488773238477"
+        });
         this.logJobPost();
         this.version = '0.0.1';
         this.location = 'Any';
