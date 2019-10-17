360314e1803008220db5cec2c71145a19faf4b2c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-10-17 16:38:48 -0700

    fix(firebase): initialize w/ the same credentials

---
+++ build/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    "projectId": "tutorbook-779d8",
+    "databaseURL": "https://tutorbook-779d8.firebaseio.com",
+    "storageBucket": "tutorbook-779d8.appspot.com",
+    "locationId": "us-central",
+    "apiKey": "AIzaSyCNaEj1Mbi-79cGA0vW48iqZtrbtU-NTh4",
+    "authDomain": "tutorbook-779d8.firebaseapp.com",
+    "messagingSenderId": "488773238477"
+});
