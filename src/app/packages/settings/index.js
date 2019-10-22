b629f0b069c06d6a7f1d69c2780f8354ff5704f5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-10-21 18:41:06 -0700

    wip(settings): enable supervisors to toggle welcome messages

--- src/app/packages/settings/index.js
+++ src/app/packages/settings/index.js
@@ -1,10 +1,32 @@
+const Data = require('data');
+
+
 // Class that provides the settings screen and header and manages all data flow
 // relating to the user's config and settings objects.
 class Settings {
 
-    constructor(app) {
-        // stub
+    constructor() {
+        this.render = window.app.render;
+        this.renderSelf();
+    }
+
+    view() {
+        window.app.intercom.view(true);
+        window.app.view(this.header, this.main, '/app/settings');
+        this.manage();
+    }
+
+    manage() {
+        this.switches = {
+            showWelcome: new MDCSwitch($(this.main).find('[id="Show welcome messages"]')[0]),
+        };
+        Object.entries(this.switches).forEach((entry) => {
+            const key = entry[0];
+            const swtch = entry[1];
+            swtch.listen('MDCSwitch:change', () => {});
+        });
     }
 };
+
 
 module.exports = Settings;
