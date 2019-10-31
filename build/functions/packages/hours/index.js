c01f6f5aecdae9244d544f0fda95e7b4894688e3
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-10-30 18:24:04 -0700

    feat(functions/hours): add service hour data flow

---
+++ build/functions/packages/hours/index.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const admin = require('firebase-admin');
+
+const updateHours = async (appt, context) => {
+    const durationInSecs = (appt.data().clockOut.sentTimestamp.toDate() -
+        appt.data().clockIn.sentTimestamp.toDate()) / 1000;
+    const user = (await admin.firestore().collection('users')
+        .doc(context.params.user).get()).data();
+
+    switch (user.type) {
+        case 'Tutor':
+            user.secondsTutored = user.secondsTutored || 0;
+            user.secondsTutored += durationInSecs;
+            break;
+        case 'Pupil':
+            user.secondsPupiled = user.secondsPupiled || 0;
+            user.secondsPupiled += durationInSecs;
+            break;
+        default:
+            return console.warn('Could not update hours for (' + user.type +
+                ') invalid user type.');
+    }
+
+    console.log('Updating (' + user.type + ') seconds for ' + user.name + '...');
+    await admin.firestore().collection('users').doc(context.params.user)
+        .update(user);
+    console.log('Updating service hour sheet...');
+    await axios({
+        method: 'get',
+        url: 'https://us-central1-tutorbook-779d8.cloudfunctions.net/updateSheet',
+    });
+    console.log('Updated ' + user.name + '\'s service hours on db and sheet.');
+};
+
+module.exports = updateHours;
