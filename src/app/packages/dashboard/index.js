22a7e50a446e7e302ee7ad82b496def94b45a357
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-12-05 11:25:50 -0800

    wip(search): add mock results

--- src/app/packages/dashboard/index.js
+++ src/app/packages/dashboard/index.js
@@ -217,18 +217,7 @@ class SupervisorDashboard extends Dashboard {
     renderSelf() {
         super.renderSelf();
         const that = this;
-
-        this.header = this.render.header('header-search', {
-            'title': 'Tutorbook',
-            'search-info': () => new NotificationDialog('About Search',
-                'Tutorbook\'s new search bar is an app-wide search feature to' +
-                ' make your job easier. From the search bar, you can find, ' +
-                'match, and message students, schedule or cancel appointments' +
-                ', and start service hour timers for tutors at your ' +
-                'location(s).', () => {}).view(),
-        });
-        MDCRipple.attachTo($(this.header).find('.search-box button')[0])
-            .unbounded = true;
+        this.renderSearch();
 
         function add(label, id) {
             $(that.main).append(
@@ -243,11 +232,66 @@ class SupervisorDashboard extends Dashboard {
         add('Everything else', 'everything');
     }
 
+    renderSearch() {
+        this.header = this.render.header('header-search', {
+            'title': 'Tutorbook',
+            'search-info': () => new NotificationDialog('About Search',
+                'Tutorbook\'s new search bar is an app-wide search feature to' +
+                ' make your job easier. From the search bar, you can find, ' +
+                'match, and message students, schedule or cancel appointments' +
+                ', and start service hour timers for tutors at your ' +
+                'location(s).', () => {}).view(),
+        });
+        MDCRipple.attachTo($(this.header).find('.search-box button')[0])
+            .unbounded = true;
+    }
+
+    renderResult(doc) { // TODO: Remove code duplication from Search()
+        const profile = doc.data();
+        const user = new User(profile);
+        const listItemData = Utils.cloneMap(profile);
+        listItemData['id'] = doc.id;
+        listItemData['go_to_user'] = () => {
+            user.view();
+        };
+
+        if (profile.payments.type === 'Paid') {
+            listItemData.type = 'Tutor';
+            listItemData.paid = true;
+            listItemData.free = false;
+            listItemData.rate = '$' + profile.payments.hourlyCharge.toFixed(0);
+            listItemData.paymentType = 'paid';
+        } else {
+            listItemData.free = true;
+            listItemData.paid = false;
+            listItemData.paymentType = 'free';
+        }
+
+        const el = this.render.template('search-result-user', listItemData);
+        Utils.replaceElement(
+            el.querySelector('.rating__meta'),
+            this.render.rating(profile.avgRating)
+        );
+        MDCRipple.attachTo(el);
+        return el;
+    }
+
+    async viewMockResults() {
+        (await firebase.firestore().collection('users').limit(5).get())
+        .forEach((doc) =>
+            $(this.header).find('#results').append(this.renderResult(doc))
+        );
+        $(this.header).find('.search-results').attr('style', 'display:visible;');
+        $(this.header).find('.search-box')
+            .addClass('search-box--elevated');
+    }
+
     viewDefaultCards() {
         super.viewDefaultCards();
         this.viewScheduleCards();
         this.viewShortcutCards();
         this.viewEverythingElse();
+        this.viewMockResults();
     }
 
     viewScheduleCards() {
