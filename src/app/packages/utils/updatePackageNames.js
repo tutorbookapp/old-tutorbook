d9b836a53397b4f8cbc64a860cbb6a88a61cb3d9
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-12-19 12:06:45 -0800

    fix(app/packages/*/package.json): add 'tutorbook-' prefix to package names

---
+++ src/app/packages/utils/updatePackageNames.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+
+function main(packages, packagesDir) { // TODO: Get packages from packagesDir
+    const packageNamesChanged = []; // Names to look for in dependencies
+    console.log('[INFO] Updating ' + packages.length + ' packages...');
+    packages.forEach((packageDir) => {
+        const filename = (packagesDir || './') + packageDir + '/package.json';
+        const packageJson = JSON.parse(fs.readFileSync(filename));
+        if (packageJson.name.indexOf('tutorbook') < 0) {
+            packageNamesChanged.push(packageJson.name);
+            packageJson.name = 'tutorbook-' + packageJson.name;
+            console.log('[DEBUG] Updating ' + packageJson.name + '...');
+            fs.writeFileSync(filename, JSON.stringify(packageJson, null, 2));
+            count++;
+        }
+    });
+    console.log('[INFO] Checked ' + packages.length + ' packages and updated ' +
+        packageNamesChanged.length + ' of those packages\' package.json file.');
+    console.log('[INFO] Searching ' + packages.length + ' packages for ' +
+        packageNamesChanged.length + ' changed package names...');
+    var count, fileCount;
+    count = fileCount = 0;
+    packages.forEach((packageDir) => {
+        const filename = (packagesDir || './') + packageDir + '/package.json';
+        const packageJson = JSON.parse(fs.readFileSync(filename));
+        const deps = packageJson.dependencies;
+        if (typeof deps !== 'object') return;
+        Object.entries(deps).forEach((dep) => {
+            if (packageNamesChanged.indexOf(dep[0]) >= 0) {
+                console.log('[DEBUG] Updating ' + dep[0] +
+                    ' dependency name in ' + packageJson.name + '...');
+                deps['tutorbook-' + dep[0]] = dep[1];
+                delete deps[dep[0]];
+                count++;
+            }
+        });
+        fs.writeFileSync(filename, JSON.stringify(packageJson, null, 2));
+        fileCount++;
+    });
+    console.log('[INFO] Updated ' + count + ' package dependency names in ' +
+        fileCount + ' package.json files.');
+};
+
+main([
+    'app',
+    'card',
+    'chats',
+    'dashboard',
+    'data',
+    'dialogs',
+    'feedback',
+    'filters',
+    'intercom',
+    'listener',
+    'login',
+    'matching',
+    'navigation',
+    'notify',
+    'payments',
+    'profile',
+    'render',
+    'schedule',
+    'schedule-card',
+    'schedule-items',
+    'search',
+    'settings',
+    'snackbar',
+    'templates',
+    'tracking',
+    'user',
+    'utils',
+], '../');
