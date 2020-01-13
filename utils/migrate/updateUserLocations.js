51e6cf34cec9f560877c5269b64b66354de97338
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-01-13 06:46:38 -0800

    fix(utils/migrate/updateUserLocations.js): add partition support

--- utils/migrate/updateUserLocations.js
+++ utils/migrate/updateUserLocations.js
@@ -8,11 +8,10 @@ admin.initializeApp({
     databaseURL: 'https://tutorbook-779d8.firebaseio.com',
 });
 
-const firestore = admin.firestore();
-
+const db = admin.firestore().collection('partitions').doc('default');
 
 function updateUser(user) {
-    return firestore.collection('users').doc(user.email).update(user).then(() => {
+    return db.collection('users').doc(user.uid).update(user).then(() => {
         console.log('[DEBUG] Updated ' + user.email + '\'s user profile doc.');
     }).catch((err) => {
         console.error('[ERROR] Error while updating ' + user.email + '\'s profile doc:', err);
@@ -22,13 +21,14 @@ function updateUser(user) {
 
 function main() {
     console.log('[INFO] Updating user\'s visibility to visible...');
-    return firestore.collection('users').get().then((snapshot) => {
+    return db.collection('users').get().then((snapshot) => {
         return snapshot.forEach((doc) => {
             var user = doc.data();
             if (user.location === 'Any' && (user.email.indexOf('pausd') > 0 ||
                     user.availability['Gunn Academic Center'] !== undefined)) {
                 user.location = 'Gunn Academic Center';
             }
+            user.uid = doc.id;
             updateUser(user);
         });
     }).then(() => {
