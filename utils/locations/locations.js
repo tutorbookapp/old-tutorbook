490a46b1d2fb6637aa0e237f88531da1435d8902
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-01-15 20:23:45 -0800

    chore(utils/locations/locations.js): update test partition data

--- utils/locations/locations.js
+++ utils/locations/locations.js
@@ -11,7 +11,7 @@ const partitions = {
     test: firestore.collection('partitions').doc('test'),
     default: firestore.collection('partitions').doc('default'),
 };
-const db = partitions.default;
+const db = partitions.test;
 
 const getSupervisor = async (params) => {
     if (params.email) return (await db.collection('users')
@@ -63,132 +63,130 @@ updateAllDaysHours([
     'Afterschool',
 ], 'fcKWyRWy124H4M34mS1r');
 
-/*
- *updateHours({
- *    Monday: [{
- *            open: 'A Period',
- *            close: 'A Period',
- *        },
- *        {
- *            open: 'B Period',
- *            close: 'B Period',
- *        },
- *        {
- *            open: 'C Period',
- *            close: 'C Period',
- *        },
- *        {
- *            open: 'F Period',
- *            close: 'F Period',
- *        },
- *        {
- *            open: '2:45 PM',
- *            close: '3:45 PM',
- *        },
- *        {
- *            open: '3:45 PM',
- *            close: '4:45 PM',
- *        },
- *    ],
- *    Tuesday: [{
- *            open: 'D Period',
- *            close: 'D Period',
- *        },
- *        {
- *            open: 'Flex',
- *            close: 'Flex',
- *        },
- *        {
- *            open: 'E Period',
- *            close: 'E Period',
- *        },
- *        {
- *            open: 'A Period',
- *            close: 'A Period',
- *        },
- *        {
- *            open: 'G Period',
- *            close: 'G Period',
- *        },
- *        {
- *            open: '3:45 PM',
- *            close: '4:45 PM',
- *        },
- *    ],
- *    Wednesday: [{
- *            open: 'B Period',
- *            close: 'B Period',
- *        },
- *        {
- *            open: 'C Period',
- *            close: 'C Period',
- *        },
- *        {
- *            open: 'D Period',
- *            close: 'D Period',
- *        },
- *        {
- *            open: 'F Period',
- *            close: 'F Period',
- *        },
- *        {
- *            open: '3:05 PM',
- *            close: '4:05 PM',
- *        },
- *        {
- *            open: '3:35 PM',
- *            close: '4:35 PM',
- *        },
- *    ],
- *    Thursday: [{
- *            open: 'E Period',
- *            close: 'E Period',
- *        },
- *        {
- *            open: 'Flex',
- *            close: 'Flex',
- *        },
- *        {
- *            open: 'B Period',
- *            close: 'B Period',
- *        },
- *        {
- *            open: 'A Period',
- *            close: 'A Period',
- *        },
- *        {
- *            open: 'G Period',
- *            close: 'G Period',
- *        },
- *        {
- *            open: '3:45 PM',
- *            close: '4:45 PM',
- *        },
- *    ],
- *    Friday: [{
- *            open: 'C Period',
- *            close: 'C Period',
- *        },
- *        {
- *            open: 'D Period',
- *            close: 'D Period',
- *        },
- *        {
- *            open: 'E Period',
- *            close: 'E Period',
- *        },
- *        {
- *            open: 'F Period',
- *            close: 'F Period',
- *        },
- *        {
- *            open: 'G Period',
- *            close: 'G Period',
- *        },
- *        {
- *            open: '3:45 PM',
- *            close: '4:45 PM',
- *        },
- *    ],
- *}, 'NJp0Y6wyMh2fDdxSuRSx');
- */
+updateHours({
+    Monday: [{
+            open: 'A Period',
+            close: 'A Period',
+        },
+        {
+            open: 'B Period',
+            close: 'B Period',
+        },
+        {
+            open: 'C Period',
+            close: 'C Period',
+        },
+        {
+            open: 'F Period',
+            close: 'F Period',
+        },
+        {
+            open: '2:45 PM',
+            close: '3:45 PM',
+        },
+        {
+            open: '3:45 PM',
+            close: '4:45 PM',
+        },
+    ],
+    Tuesday: [{
+            open: 'D Period',
+            close: 'D Period',
+        },
+        {
+            open: 'Flex',
+            close: 'Flex',
+        },
+        {
+            open: 'E Period',
+            close: 'E Period',
+        },
+        {
+            open: 'A Period',
+            close: 'A Period',
+        },
+        {
+            open: 'G Period',
+            close: 'G Period',
+        },
+        {
+            open: '3:45 PM',
+            close: '4:45 PM',
+        },
+    ],
+    Wednesday: [{
+            open: 'B Period',
+            close: 'B Period',
+        },
+        {
+            open: 'C Period',
+            close: 'C Period',
+        },
+        {
+            open: 'D Period',
+            close: 'D Period',
+        },
+        {
+            open: 'F Period',
+            close: 'F Period',
+        },
+        {
+            open: '3:05 PM',
+            close: '4:05 PM',
+        },
+        {
+            open: '3:35 PM',
+            close: '4:35 PM',
+        },
+    ],
+    Thursday: [{
+            open: 'E Period',
+            close: 'E Period',
+        },
+        {
+            open: 'Flex',
+            close: 'Flex',
+        },
+        {
+            open: 'B Period',
+            close: 'B Period',
+        },
+        {
+            open: 'A Period',
+            close: 'A Period',
+        },
+        {
+            open: 'G Period',
+            close: 'G Period',
+        },
+        {
+            open: '3:45 PM',
+            close: '4:45 PM',
+        },
+    ],
+    Friday: [{
+            open: 'C Period',
+            close: 'C Period',
+        },
+        {
+            open: 'D Period',
+            close: 'D Period',
+        },
+        {
+            open: 'E Period',
+            close: 'E Period',
+        },
+        {
+            open: 'F Period',
+            close: 'F Period',
+        },
+        {
+            open: 'G Period',
+            close: 'G Period',
+        },
+        {
+            open: '3:45 PM',
+            close: '4:45 PM',
+        },
+    ],
+}, 'NJp0Y6wyMh2fDdxSuRSx');
