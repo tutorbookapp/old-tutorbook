cadebad6f4fca4aa1ff341ecf49e85ff13f1f189
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-01-22 12:30:59 -0800

    chore(utils/algolia/kickstart.js): add location.id and partition to chats facetFilters

--- utils/algolia/kickstart.js
+++ utils/algolia/kickstart.js
@@ -1,11 +1,7 @@
 // Script that adds all relevant objects in Firestore to Algolia indexes
 
-/*
- *const id = 'TODO: ADD-ALGOLIA-APP-ID-HERE';
- *const key = 'TODO: ADD-ALGOLIA-KEY-HERE';
- */
-const id = '9FGZL7GIJM';
-const key = 'fb7894893d554015714ca2246b9f2b04';
+const id = 'TODO: ADD-ALGOLIA-APP-ID-HERE';
+const key = 'TODO: ADD-ALGOLIA-KEY-HERE';
 const client = require('algoliasearch')(id, key);
 const serviceAccount = require('../admin-cred.json');
 const admin = require('firebase-admin');
@@ -37,22 +33,29 @@ async function main() {
      *(await db.collection('users').get()) // USERS
      *.forEach((doc) => algolia(doc, 'users'));
      */
-    (await db.collection('locations').get()).forEach(async (doc) => { // APPTS
-        (await doc.ref.collection('appointments').get()) // UPCOMING
-        .forEach((doc) => algolia(doc, 'appts', {
-            attributesForFaceting: [
-                'filterOnly(location.id)',
-                'filterOnly(partition)',
-            ],
-        }));
-    });
+    /*
+     *(await db.collection('locations').get()).forEach(async (doc) => { // APPTS
+     *    (await doc.ref.collection('appointments').get()) // UPCOMING
+     *    .forEach((doc) => algolia(doc, 'appts', {
+     *        attributesForFaceting: [
+     *            'filterOnly(location.id)',
+     *            'filterOnly(partition)',
+     *        ],
+     *    }));
+     *});
+     */
     //(await doc.ref.collection('activeAppointments').get()) // ACTIVE
     //.forEach((doc) => algolia(doc, 'activeAppts'));
     //(await doc.ref.collection('pastAppointments').get()) // PAST
     //.forEach((doc) => algolia(doc, 'pastAppts'));
     //});
-    //(await db.collection('chats').get()) // CHATS
-    //.forEach((doc) => algolia(doc, 'chats'));
+    (await db.collection('chats').get()) // CHATS
+    .forEach((doc) => algolia(doc, 'chats', {
+        attributesForFaceting: [
+            'filterOnly(location.id)',
+            'filterOnly(partition)',
+        ],
+    }));
 }
 
 main();
