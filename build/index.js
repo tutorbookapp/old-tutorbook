873242dd43f4c5694e49fc89bf745eab5e329ba1
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-02-01 16:43:23 -0800

    fix(build): update app redirection

--- build/index.js
+++ build/index.js
@@ -1,23 +1,17 @@
-if (window.location.toString().split('/').indexOf('login') >= 0)
-    return window.location.replace('/app');
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) { // TODO: Replace 'Get Started' button with 'Start App' button. 
-        // If there is a user, redirect to the app or whatever page within
-        // the app we're pointed to. NOTE: We have to use URL parameters as
-        // we haven't initialized the router yet.
-        const splitLocation = window.location.toString().split('/');
-        if (splitLocation.indexOf('app') < 0) {
-            // URL does not point to a location within the app.
-            window.location.replace('/app');
-        } else {
-            // Send URL location to app via URL parameters.
-            var locationParameter = "/app/?redirect=";
-            splitLocation.forEach((str) => {
-                if (splitLocation.indexOf(str) > splitLocation.indexOf('app')) {
-                    locationParameter += str + '/';
-                }
-            });
-            window.location.replace(locationParameter);
+const splitLocation = window.location.toString().split('/');
+if (splitLocation.indexOf('app') < 0) {
+    // URL does not point to a location within the app.
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // TODO: Replace 'Get Started' button with 'Start App' button. 
         }
-    }
-});
+    });
+} else {
+    // Send URL location to app via URL parameters.
+    var locationParameter = "/app/?redirect=";
+    splitLocation.forEach((str) => {
+        if (splitLocation.indexOf(str) > splitLocation.indexOf('app'))
+            locationParameter += str + '/';
+    });
+    window.location.replace(locationParameter);
+}
