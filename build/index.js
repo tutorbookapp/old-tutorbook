5c2fdec357e6cb3b8c39d5e03368fe0dc93890e5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-02-01 16:54:55 -0800

    chore(build): update site build from src assets

--- build/index.js
+++ build/index.js
@@ -1,17 +1,123 @@
-const splitLocation = window.location.toString().split('/');
-if (splitLocation.indexOf('app') < 0) {
-    // URL does not point to a location within the app.
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            // TODO: Replace 'Get Started' button with 'Start App' button. 
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Site = function () {
+    function Site() {
+        _classCallCheck(this, Site);
+
+        this.redirect();
+    }
+
+    _createClass(Site, [{
+        key: 'redirect',
+        value: function redirect() {
+            var splitLocation = window.location.toString().split('/');
+            if (splitLocation.indexOf('app') < 0) {
+                // URL does not point to a location within the app.
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (user) {
+                        // TODO: Replace 'Get Started' with 'Start App' button. 
+                    }
+                });
+            } else {
+                // Send URL location to app via URL parameters.
+                var locationParameter = "/app/?redirect=";
+                splitLocation.forEach(function (str) {
+                    if (splitLocation.indexOf(str) > splitLocation.indexOf('app')) locationParameter += str + '/';
+                });
+                window.location.replace(locationParameter);
+            }
         }
-    });
-} else {
-    // Send URL location to app via URL parameters.
-    var locationParameter = "/app/?redirect=";
-    splitLocation.forEach((str) => {
-        if (splitLocation.indexOf(str) > splitLocation.indexOf('app'))
-            locationParameter += str + '/';
-    });
-    window.location.replace(locationParameter);
-}
+    }]);
+
+    return Site;
+}();
+
+window.onload = function () {
+    return window.site = new Site();
+};
+
+module.exports = Site;
+
+/***/ })
+/******/ ]);
