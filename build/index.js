b3818e6a5d242a13462691a7b62c22f9dfc0e266
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-02-01 16:33:51 -0800

    wip(build/index*): clean up and revamp website

--- build/index.js
+++ build/index.js
@@ -1,29 +1,23 @@
-window.onload = function() {
-    if (window.location.toString().split('/').indexOf('login') >= 0) {
-        return window.location.replace('/app');
-    }
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            // If there is a user, redirect to the app or whatever page within
-            // the app we're pointed to. NOTE: We have to use URL parameters as
-            // we haven't initialized the router yet.
-            const splitLocation = window.location.toString().split('/');
-            if (splitLocation.indexOf('app') < 0) {
-                // URL does not point to a location within the app.
-                window.location.replace('/app');
-            } else {
-                // Send URL location to app via URL parameters.
-                var locationParameter = "/app/?redirect=";
-                splitLocation.forEach((str) => {
-                    if (splitLocation.indexOf(str) > splitLocation.indexOf('app')) {
-                        locationParameter += str + '/';
-                    }
-                });
-                window.location.replace(locationParameter);
-            }
+if (window.location.toString().split('/').indexOf('login') >= 0)
+    return window.location.replace('/app');
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) { // TODO: Replace 'Get Started' button with 'Start App' button. 
+        // If there is a user, redirect to the app or whatever page within
+        // the app we're pointed to. NOTE: We have to use URL parameters as
+        // we haven't initialized the router yet.
+        const splitLocation = window.location.toString().split('/');
+        if (splitLocation.indexOf('app') < 0) {
+            // URL does not point to a location within the app.
+            window.location.replace('/app');
         } else {
-            // If not, show the website.
-            window.location.replace('/site');
+            // Send URL location to app via URL parameters.
+            var locationParameter = "/app/?redirect=";
+            splitLocation.forEach((str) => {
+                if (splitLocation.indexOf(str) > splitLocation.indexOf('app')) {
+                    locationParameter += str + '/';
+                }
+            });
+            window.location.replace(locationParameter);
         }
-    });
-};
+    }
+});
