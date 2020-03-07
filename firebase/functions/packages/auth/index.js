6ac30c4eb9f38e9568a1160e27f7272922ebc929
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-03-07 12:48:37 -0800

    feat(functions/auth): add accessId support and jsdocs

--- firebase/functions/packages/auth/index.js
+++ firebase/functions/packages/auth/index.js
@@ -25,48 +25,66 @@ const customAuth = (req, res) => {
     });
 };
 
+/**
+ * A change to a Firestore document containing snapshots of before and after the
+ * change.
+ * @typedef {Object} Change
+ * @property {DocumentSnapshot} before - The document's snapshot before the 
+ * change was enacted.
+ * @property {DocumentSnapshot} after - The document's snapshot after the change
+ * was enacted.
+ */
 
+/**
+ * Updates user Firebase Authentication claims to match their user documents.
+ * @param {Change} change - The changed (written) Firestore user document.
+ * @param {Context} context - The path (in JSON form) of the Firestore user
+ * document that was changed.
+ * @return {Promise<undefined>} Promise that resolves once the user's Firebase
+ * Authentication claims have been updated.
+ */
 const updateAuth = async (change, context) => {
+
+    // 0) Fetch data and return if the user was deleted. TODO: Remove Firebase
+    // Authentication account if the user document was deleted.
     const profile = change.after.data();
     const uid = context.params.id;
-    if (!profile)
-        return console.warn('[WARNING] User (' + uid + ') doc was deleted.');
+    if (!profile) return console.warn('[WARNING] User (' + uid + ') deleted.');
     const db = admin.firestore().collection('partitions').doc('default');
 
-    // Check to see if the supervisor's id is in the codes collection
+    // 1) Check to see if the supervisor's id is in the codes collection.
     const codes = await db.collection('auth').doc('supervisors').get();
     if (!codes.exists) throw new Error('Supervisor codes did not exist.');
     const validIDs = Object.keys(codes.data());
 
+    // 2a) If the user is, add the supervisor custom auth claims.
     if (profile.type === 'Supervisor' && profile.authenticated &&
-        validIDs.indexOf(uid) >= 0) { // SUPERVISOR
+        validIDs.indexOf(uid) >= 0) {
         console.log('[DEBUG] ' + profile.name + ' was a verified supervisor. ' +
-            'Adding customAuth claims...');
-        const locations = await db.collection('locations')
-            .where('supervisors', 'array-contains', uid)
-            .get();
+            'Adding custom auth claims...');
         return admin.auth().setCustomUserClaims(uid, {
             supervisor: true,
-            parent: false,
-            locations: locations.docs.map(doc => doc.id),
-            children: [],
-        }).then(() => console.log('[DEBUG] Added supervisor customAuth to ' +
+            locations: (await db.collection('locations')
+                .where('supervisors', 'array-contains', uid)
+                .get()).docs.map(doc => doc.id),
+            access: profile.access || [],
+        }).then(() => console.log('[DEBUG] Added supervisor custom auth to ' +
             profile.name + '\'s account.')).catch((err) => console.error(
-            '[ERROR] Could not add supervisor customAuth to ' + profile.name +
+            '[ERROR] Could not add supervisor custom auth to ' + profile.name +
             '\'s account b/c of ' + err.message));
-    } else { // NOTHING
-        console.log('[DEBUG] ' + profile.name + ' was not a verified ' +
-            'supervisor. Ensuring that they don\'t have customAuth claims...');
-        return admin.auth().setCustomUserClaims(uid, {
-            supervisor: false,
-            parent: false,
-            locations: [],
-            children: [],
-        }).then(() => console.log('[DEBUG] Removed any customAuth claims from' +
-            ' ' + profile.name + '\'s account.')).catch((e) => console.error(
-            '[ERROR] Could not remove customAuth claims from ' + profile.name +
-            '\'s account b/c of ' + e.message));
     }
+
+    // 2b) If the user isn't, remove the supervisor custom auth claims.
+    console.log('[DEBUG] ' + profile.name + ' was not a verified ' +
+        'supervisor. Ensuring that they don\'t have custom auth claims...');
+    return admin.auth().setCustomUserClaims(uid, {
+        supervisor: false,
+        locations: [],
+        access: profile.access || [],
+    }).then(() => console.log('[DEBUG] Removed any custom auth claims from' +
+        ' ' + profile.name + '\'s account.')).catch((e) => console.error(
+        '[ERROR] Could not remove custom auth claims from ' + profile.name +
+        '\'s account b/c of ' + e.message));
 };
 
 
