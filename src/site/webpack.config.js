3dc81e7858860528f953328abda4abbe67545966
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-03-29 13:00:29 -0700

    wip(src/site): update webpack config and loaders
    Temporary fix for #25.

--- src/site/webpack.config.js
+++ src/site/webpack.config.js
@@ -2,18 +2,58 @@ const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = (env, argv) => [{
-    entry: './packages/site/index.js',
+    watch: argv.mode === 'development',
+    devtool: argv.mode === 'development' ? 'eval-source-map' : 'none',
+    entry: path.resolve(__dirname, 'packages/site/index.js'),
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../../build/'),
     },
+    resolve: {
+        extensions: ['.scss', '.js'],
+    },
     module: {
         rules: [{
             test: /\.s[ac]ss$/i,
-            use: ['to-string-loader', 'css-loader', 'sass-loader'],
+            use: ['to-string-loader', 'css-loader', {
+                loader: 'sass-loader',
+                options: {
+                    implementation: require('sass'),
+                    sassOptions: {
+                        fiber: require('fibers'),
+                        includePaths: [ // TODO: See #52.
+                            path.resolve(__dirname, 'packages/error-msg/' +
+                                'node_modules'),
+                            path.resolve(__dirname, 'node_modules'),
+                        ],
+                    },
+                },
+            }],
         }, {
             test: /\.html$/i,
-            use: ['to-string-loader', 'html-loader'],
+            use: ['to-string-loader', {
+                loader: 'html-loader',
+                options: {
+                    esModule: true,
+                    minimize: argv.mode === 'production' ? {
+                        collapseBooleanAttributes: true,
+                        collapseWhitespace: true,
+                        decodeEntities: true,
+                        minifyCSS: true,
+                        minifyJS: true,
+                        quoteCharacter: '"',
+                        removeComments: true,
+                        removeOptionalTags: true,
+                        removeRedundantAttributes: true,
+                        removeScriptTypeAttributes: true,
+                        removeStyleLinkTypeAttributes: true,
+                        removeTagWhitespace: true,
+                        sortAttributes: true,
+                        sortClassName: true,
+                        useShortDoctype: true,
+                    } : false,
+                },
+            }],
         }, {
             test: /\.svg$/i,
             use: 'svg-url-loader',
@@ -30,8 +70,8 @@ module.exports = (env, argv) => [{
             },
         })],
     },
-    watch: argv.mode === 'development',
 }, {
+    watch: argv.mode === 'development',
     entry: './packages/site/index.scss',
     output: {
         filename: 'index.css.js', // TODO: Don't generate this unused JS file.
@@ -54,5 +94,4 @@ module.exports = (env, argv) => [{
             }],
         }],
     },
-    watch: argv.mode === 'development',
 }];
